8291690c5f761cc9a49ec2c34609bee5e6c58387
MutiatBash <mutiatbashua@gmail.com>
2022-11-16 15:59:52 -0800

    env file

---
+++ env.c
@@ -0,0 +1,55 @@
+#include "shell.h"
+
+/**
+ * _myenv - prints the current environment
+ * @info: structure containing potential arguements. used to maintain
+ *
+ * Return: Always 0
+ */
+int _myenv(info_t *info)
+{
+	print_list_str(info->env);
+	return (0);
+}
+
+/**
+ * _getenv - gets the value of an environment variable
+ * @info: structure containing potential arguements
+ * @name: env var name
+ *
+ * Return: the value
+ */
+char *_getenv(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
+
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * _mysetenv - initialize a new environment variable
+ * or modify an existing one
+ * @info: struct contsaining potential arguments. used to maintain constant
+ * funtion prototype
+ * Return: Always 0
+ */
+int _mysetenv(info_t *info)
+{
+	if (info->argc != 3)
+	{
+		_eputs("Incorrect number of arguments\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
+
