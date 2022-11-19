043a6f4fce6984e57f9428fbd4e963a06dc63c1d
MutiatBash <mutiatbashua@gmail.com>
2022-11-19 13:00:40 -0800

    env1

--- env.c
+++ env.c
@@ -53,3 +53,40 @@ int _mysetenv(info_t *info)
 	return (1);
 }
 
+/**
+ * _myunsetenv - remove an environment variable
+ * @info: structure containing potential arguements
+ *
+ * Return: Always 0
+ */
+int _myunsetenv(info_t *info)
+{
+	int i;
+
+	if (info->argc == 1)
+	{
+		_eputs("Too ew arguements.\n");
+		return (1);
+	}
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
+
+	return (0);
+}
+
+/**
+ * populate_env_list - populates env linked list
+ * @info: structure containing potential arguements
+ *
+ * Return: Always 0
+ */
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
+	return (0);
+}
